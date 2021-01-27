bd6565a008e270fea7001d929bdde4b0e46f1779
Jake Ledoux <contactjakeledoux@gmail.com>
2021-01-27 18:39:49 -0500

    RFC3339 date-string parsing function

--- wavyfm/util.py
+++ wavyfm/util.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 __all__ = ["ENV_VARS", "API_BASE_DEFAULT", "API_BASE_V1"]
 
 """The v1 API base URL"""
@@ -11,3 +13,10 @@ ENV_VARS = {
     "client_id": "WAVYFM_CLIENT_ID",
     "client_secret": "WAVYFM_CLIENT_SECRET",
 }
+
+
+def datetime_from_string(raw_date: str) -> datetime:
+    """
+    Creates a UTC DateTime object from a RFC3339 formatted string.
+    """
+    return datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%S.%f%z')
